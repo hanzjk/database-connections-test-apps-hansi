0759f0e71ad0d3bd49ea4eaee118f1230c13ca2d
hanzjk <hansi@wso2.com>
2024-11-02 21:02:03 +0530

    Setup repository

---
+++ database-connection-test/main.go
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com/) All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/database-configs", databaseConfigs)
+
+	serverPort := 9090
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", serverPort),
+		Handler: serverMux,
+	}
+	go func() {
+		log.Printf("Starting HTTP Service on port %d\n", serverPort)
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("HTTP ListenAndServe error: %v", err)
+		}
+		log.Println("HTTP server stopped serving new requests.")
+	}()
+
+	stopCh := make(chan os.Signal, 1)
+	signal.Notify(stopCh, syscall.SIGINT, syscall.SIGTERM)
+	<-stopCh // Wait for shutdown signal
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	log.Println("Shutting down the server...")
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("HTTP shutdown error: %v", err)
+	}
+	log.Println("Shutdown complete.")
+}
+
+func databaseConfigs(w http.ResponseWriter, r *http.Request) {
+	// Read environment variables
+	host := os.Getenv("HOST_NAME")
+	port := os.Getenv("PORT")
+	username := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
+	database := os.Getenv("DATABASE_NAME")
+
+	// Prepare a JSON response with the environment variables
+	envVars := map[string]string{
+		"host":         host,
+		"port":       port,
+		"username": username,
+		"password":    password,
+		"database":    database,
+	}
+
+	// Set the content type to JSON and write the response
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(envVars); err != nil {
+		http.Error(w, fmt.Sprintf("Failed to encode response: %v", err), http.StatusInternalServerError)
+	}
+	
+	
+}
+
